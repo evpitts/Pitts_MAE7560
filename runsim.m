eebf4064383d1295fb29c509353eada2690aceae
Emily Pitts <evpitts35@gmail.com>
2020-10-30 18:07:40 -0600

    Modifying runsim

--- runsim.m
+++ runsim.m
@@ -9,19 +9,15 @@ nstep_aid = ceil(simpar.general.tsim/simpar.general.dt_kalmanUpdate);
 t = (0:nstep-1)'*simpar.general.dt;
 t_kalman = (0:nstep_aid)'.*simpar.general.dt_kalmanUpdate;
 nstep_aid = length(t_kalman);
-%Acceleration due to moon gravity
-r_i_reference = [0, 0, simpar.general.R_M]';
-a_grav = -simpar.general.MU/norm(r_i_reference)^3*r_i_reference;
 %If you are computing the nominal star tracker or other sensor orientations
 %below is an example of one way to do this
-qz = rotv2quat(simpar.general.thz_st,[0,0,1]');
-qy = rotv2quat(simpar.general.thy_st,[0,1,0]');
-qx = rotv2quat(simpar.general.thx_st,[1,0,0]');
-simpar.general.q_b2st_nominal = qmult(qx,qmult(qy,qz));
 qz = rotv2quat(simpar.general.thz_c,[0,0,1]');
 qy = rotv2quat(simpar.general.thy_c,[0,1,0]');
 qx = rotv2quat(simpar.general.thx_c,[1,0,0]');
 simpar.general.q_b2c_nominal = qmult(qx,qmult(qy,qz));
+%Acceleration due to moon gravity
+r_i_reference = [0, 0, simpar.general.R_M]';
+a_grav = -simpar.general.MU/norm(r_i_reference)^3*r_i_reference;
 %% Pre-allocate buffers for saving data
 % Truth, navigation, and error state buffers
 x_buff          = zeros(simpar.states.nx,nstep);
@@ -35,15 +31,16 @@ ytilde_buff     = zeros(simpar.general.n_inertialMeas,nstep);
 res_example          = zeros(3,nstep_aid);
 resCov_example       = zeros(3,3,nstep_aid);
 K_example_buff       = zeros(simpar.states.nxfe,3,nstep_aid);
-accel_buff           = zeros(3,nstep_aid);
-cam_buff             = zeros(2,nstep_aid);
+loss_res = zeros(2, nstep_aid);
+% accel_buff           = zeros(3,nstep_aid);
+% cam_buff             = zeros(2,nstep_aid);
 %% Initialize the navigation covariance matrix
 % P_buff(:,:,1) = initialize_covariance();
 %% Initialize the truth state vector
 %Does need simpar
 x_buff(:,1) = initialize_truth_state(simpar);
-disp('x_buff')
-disp(x_buff(:,1))
+% disp('x_buff')
+% disp(x_buff(:,1))
 %% Initialize the navigation state vector
 %Should be (x_buff,simpar)
 xhat_buff(:,1) = initialize_nav_state(x_buff(:,1),simpar);
@@ -87,16 +84,19 @@ for i=2:nstep
     %   Realize a sample of process noise (don't forget to scale Q by 1/dt!)
     %   Define any inputs to the truth state DE
     %   Perform one step of RK4 integration
-    vperp = abs(x_buff(simpar.states.ix.vel(1),i-1));
-    Q_g = 2*vperp*simpar.truth.params.sig_grav_ss^2/simpar.general.d_g;
-    w_g = sqrt(Q_g/simpar.general.dt)*randn(2,1);
-    w_a = sqrt(simpar.truth.params.Q_nongra/simpar.general.dt)*randn(2,1);
-    w_p = sqrt(Q_p/simpar.general.dt).*randn(length(simpar.states.ix.param),1);
-    input_truth.w_g = w_g;
-    input_truth.w_a = w_a;
-    input_truth.w_p = w_p;
-    input_truth.a_grav = a_grav;
-    input_truth.a_thr = a_thr(:,i-1);
+%     vperp = abs(x_buff(simpar.states.ix.vel(1),i-1));
+%     Q_g = 2*vperp*simpar.truth.params.sig_grav_ss^2/simpar.general.d_g;
+%     w_g = sqrt(Q_g/simpar.general.dt)*randn(2,1);
+%     w_a = sqrt(simpar.truth.params.Q_nongra/simpar.general.dt)*randn(2,1);
+%     w_p = sqrt(Q_p/simpar.general.dt).*randn(length(simpar.states.ix.param),1);
+%     input_truth.w_g = w_g;
+    input_truth.w_a = 0;
+    input_truth.w_r = 0;
+    input_truth.w_d = 0;
+    input_truth.w_h = 0;
+    input_truth.w_accl = 0;
+%     input_truth.a_grav = a_grav;
+%     input_truth.a_thr = a_thr(:,i-1);
     input_truth.simpar = simpar;
     %Perform one step of RK4 integration
     x_buff(:,i) = rk4('truthState_de', x_buff(:,i-1), input_truth,...
@@ -107,17 +107,19 @@ for i=2:nstep
     %   Assign inputs to the navigation state DE
     %   Perform one step of RK4 integration
     input_nav.ytilde = ytilde_buff(:,i);
+    input_nav.v_perp = x_buff(simpar.states.ix.vel(2),i-1);
     input_nav.simpar = simpar;
-    input_nav.x = x_buff(:,i-1);
-    input_nav.a_grav = a_grav;
+%     input_nav.x = x_buff(:,i-1);
+%     input_nav.a_grav = a_grav;
     xhat_buff(:,i) = rk4('navState_de', xhat_buff(:,i-1), input_nav, ...
         simpar.general.dt);
     % Propagate the covariance to t_n
     input_cov.simpar = simpar;
-    input_cov.x = x_buff(:,i-1);
-    input_cov.xhat = xhat_buff(:,i-1);
-    P_buff(:,:,i) = rk4('navCov_de', P_buff(:,:,i-1), input_cov, ...
-        simpar.general.dt);
+    input_cov.ytilde = [];
+%     input_cov.x = x_buff(:,i-1);
+%     input_cov.xhat = xhat_buff(:,i-1);
+%     P_buff(:,:,i) = rk4('navCov_de', P_buff(:,:,i-1), input_cov, ...
+%         simpar.general.dt);
     % Propagate the error state from tn-1 to tn if errorPropTestEnable == 1
     if simpar.general.errorPropTestEnable
         input_delx.xhat = xhat_buff(:,i-1);
@@ -148,24 +150,28 @@ for i=2:nstep
         %       Estimate the error state vector
         %       Update and save the covariance matrix
         %       Correct and save the navigation states
-        ztilde_example = example.synthesize_measurement();
-        ztildehat_example = example.predict_measurement();
-        H_example = example.compute_H();
-        example.validate_linearization();
-        res_example(:,k) = example.compute_residual();
-        resCov_example(:,k) = compute_residual_cov();
-        K_example_buff(:,:,k) = compute_Kalman_gain();
-        del_x = estimate_error_state_vector();
-        P_buff(:,:,k) = update_covariance();
-        xhat_buff(:,i) = correctErrors();
+% % %         ztilde_example = example.synthesize_measurement();
+% % %         ztildehat_example = example.predict_measurement();
+% % %         H_example = example.compute_H();
+% % %         example.validate_linearization();
+% % %         res_example(:,k) = example.compute_residual();
+% % %         resCov_example(:,k) = compute_residual_cov();
+% % %         K_example_buff(:,:,k) = compute_Kalman_gain();
+% % %         del_x = estimate_error_state_vector();
+% % %         P_buff(:,:,k) = update_covariance();
+% % %         xhat_buff(:,i) = correctErrors();
+        [loss_ztilde, r_fi] = loss.synth_measurement(x_buff(:,1),simpar);
+        loss_ztildehat = loss.pred_measurement(xhat_buff(:,1),r_fi,simpar);
+        loss_res(:,k) = loss_ztilde-loss_ztildehat;
     end
     
     %Recalculate a_thr by calling the guidance law again
-    a_thr(:,1) = guidance(x_buff(simpar.states.ix.pos,1),...
+a_thr(:,1) = guidance(x_buff(simpar.states.ix.pos,1),...
     x_buff(simpar.states.ix.vel,1),...
     [simpar.general.astar_x_tf, simpar.general.astar_y_tf, simpar.general.astar_x_tf]',...
     [simpar.general.vstar_x_tf, simpar.general.vstar_y_tf, simpar.general.vstar_z_tf]',...
-    [simpar.general.rstar_x_tf, 4, 1]',simpar.general.tsim,...
+    [simpar.general.rstar_x_tf, simpar.general.rstar_y_tf, simpar.general.rstar_z_tf]',...
+    simpar.general.tsim,...
     a_grav, 'apollo');
 
     if verbose && mod(i,100) == 0
